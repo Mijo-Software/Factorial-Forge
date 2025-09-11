2eda4ff9068f9893042fd7f48ac34490b31cba2b
Michael de Radobýl <mjohne@users.noreply.github.com>
2025-09-11 23:16:01 +0200

    Add files via upload

---
+++ Factorializer.cs
@@ -0,0 +1,21 @@
+﻿namespace FactorialForge
+{
+	internal class Factorializer
+	{
+		public static long Factorial(int n)
+		{
+			if (n < 0)
+			{
+				throw new ArgumentException(message: "Factorial is only defined for non-negative integers.");
+			}
+
+			long result = 1;
+			for (int i = 2; i <= n; i++)
+			{
+				result *= i;
+			}
+			return result;
+		}
+
+	}
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace FactorialForge
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl = new TabControl();
+			this.tabPageFactorial = new TabPage();
+			this.labelFactorial = new Label();
+			this.numericUpDownFactorial = new NumericUpDown();
+			this.tabControl.SuspendLayout();
+			this.tabPageFactorial.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)this.numericUpDownFactorial).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tabControl
+			// 
+			this.tabControl.Appearance = TabAppearance.Buttons;
+			this.tabControl.Controls.Add(this.tabPageFactorial);
+			this.tabControl.Dock = DockStyle.Fill;
+			this.tabControl.Location = new Point(0, 0);
+			this.tabControl.Name = "tabControl";
+			this.tabControl.SelectedIndex = 0;
+			this.tabControl.ShowToolTips = true;
+			this.tabControl.Size = new Size(253, 95);
+			this.tabControl.TabIndex = 0;
+			// 
+			// tabPageFactorial
+			// 
+			this.tabPageFactorial.Controls.Add(this.labelFactorial);
+			this.tabPageFactorial.Controls.Add(this.numericUpDownFactorial);
+			this.tabPageFactorial.Location = new Point(4, 27);
+			this.tabPageFactorial.Name = "tabPageFactorial";
+			this.tabPageFactorial.Padding = new Padding(3);
+			this.tabPageFactorial.Size = new Size(245, 64);
+			this.tabPageFactorial.TabIndex = 0;
+			this.tabPageFactorial.Text = "Factorial";
+			this.tabPageFactorial.UseVisualStyleBackColor = true;
+			// 
+			// labelFactorial
+			// 
+			this.labelFactorial.AutoSize = true;
+			this.labelFactorial.Location = new Point(6, 32);
+			this.labelFactorial.Name = "labelFactorial";
+			this.labelFactorial.Size = new Size(38, 15);
+			this.labelFactorial.TabIndex = 1;
+			this.labelFactorial.Text = "label1";
+			// 
+			// numericUpDownFactorial
+			// 
+			this.numericUpDownFactorial.Location = new Point(6, 6);
+			this.numericUpDownFactorial.Maximum = new decimal(new int[] { 20, 0, 0, 0 });
+			this.numericUpDownFactorial.Name = "numericUpDownFactorial";
+			this.numericUpDownFactorial.Size = new Size(69, 23);
+			this.numericUpDownFactorial.TabIndex = 0;
+			this.numericUpDownFactorial.TextAlign = HorizontalAlignment.Center;
+			this.numericUpDownFactorial.ValueChanged += this.NumericUpDownFactorial_ValueChanged;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new SizeF(7F, 15F);
+			this.AutoScaleMode = AutoScaleMode.Font;
+			this.ClientSize = new Size(253, 95);
+			this.Controls.Add(this.tabControl);
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = FormStartPosition.CenterScreen;
+			this.Text = "Factorial Forge";
+			this.tabControl.ResumeLayout(false);
+			this.tabPageFactorial.ResumeLayout(false);
+			this.tabPageFactorial.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)this.numericUpDownFactorial).EndInit();
+			this.ResumeLayout(false);
+		}
+
+		#endregion
+
+		private TabControl tabControl;
+		private TabPage tabPageFactorial;
+		private NumericUpDown numericUpDownFactorial;
+		private Label labelFactorial;
+	}
+}
